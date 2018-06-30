ff2463ee38fb87400bc35b2d370188d55c0defd6
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-30 05:41:25 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,20 +1,6 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort do |x, y|
-    i = 0
-    a = esperanto.index(x.chars[i])
-    b = esperanto.index(y.chars[i])
-    looping = true
-    while looping
-      if a < b || b > a 
-        looping = false
-        a <=> b
-      else
-        i += 1
-      end
-    end
-  end
+  arr.sort_by
 end
 arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
